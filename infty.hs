a836c2637cb0c93d4d1815d05b60474a8b72fb04
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-04-30 15:32:36 +0200

    Define MULTIPLY and DIVIDE (.*. and ./.)

--- infty.hs
+++ infty.hs
@@ -13,3 +13,17 @@ negateInfty (a, b) = (-a, b)
 
 (.-.) :: Infty -> Infty -> Infty
 x .-. y = x .+. negateInfty y
+
+(.*.) :: Infty -> Infty -> Infty
+(0, 0) .*. (0, 0) = (0, 0)
+(0, _) .*. (0, _) = error "Had a (zero, non-zero)" -- Because it's easy
+(a, b) .*. (0,0) = (a, b-1)
+(0, 0) .*. (a,b) = (a, b-1) -- Ugly, but I don't know any better
+(a_0, b_0) .*. (a_1, b_1) = (a_0 * a_1, b_0 + b_1)
+
+recipInfty :: Infty -> Infty
+recipInfty (0, 0) = (1, 1)
+recipInfty (a, b) = (1/a, -b)
+
+(./.) :: Infty -> Infty -> Infty
+x ./. y = x .*. recipInfty y
