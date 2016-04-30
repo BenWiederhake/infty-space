bd67c4eae646f49af3592f57540317eb314a9dda
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2016-04-30 15:07:07 +0200

    Define PLUS (.+.)

---
+++ infty.hs
@@ -0,0 +1,9 @@
+type Infty = (Float, Int)
+
+(.+.) :: Infty -> Infty -> Infty
+(a_0, b_0) .+. (a_1, b_1) =
+    case compare b_0 b_1 of
+        LT -> (a_1, b_1)
+        EQ -> let a_sum = (a_0 + a_1)
+              in  if a_sum == 0 then (0,0) else (a_sum, b_0)
+        GT -> (a_0, b_0)
